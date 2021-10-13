93b41c84b923440a27fcc31b3c7e457976d72b68
mwood77 <43637076+mwood77@users.noreply.github.com>
2021-10-14 01:45:59 +0200

    udpate gitignore x2

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AnalyticsService } from '../utils/analytics.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialImportModule } from './material-import/material-import.module';
+import { TableComponent } from './table/table.component';
+
+import { API_KEY, GoogleSheetsDbService } from 'ng-google-sheets-db';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableComponent
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialImportModule,
+  ],
+  providers: [
+    AnalyticsService,
+    {
+      provide: API_KEY,
+      // @todo - key comes from repo secrets
+      useValue: ""
+    },
+    GoogleSheetsDbService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
